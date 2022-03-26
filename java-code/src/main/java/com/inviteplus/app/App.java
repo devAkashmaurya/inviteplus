f87f649f271cb130700e1a57ddf1dd3410fefefc
devAkashmaurya <dev.akashmaurya@gmail.com>
2022-03-26 18:12:36 +0000

    added java code with mvn

---
+++ java-code/src/main/java/com/inviteplus/app/App.java
@@ -0,0 +1,13 @@
+package com.inviteplus.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! by inviteplus akash" );
+    }
+}
---
+++ java-code/src/test/java/com/inviteplus/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.inviteplus.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
